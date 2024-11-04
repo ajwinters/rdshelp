7f3f9a17b6be72982258b8f4fa6325d1ca09d4f8
Alex Winters <awin117@gmail.com>
2024-11-04 09:22:37 -0500

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rdshelp",
+    version="1.0.0",
+    packages=find_packages(),
+    py_modules=["rdshelp"],  # Name of your Python file
+    install_requires=["psycopg2","numpy","pandas","re"],  # Add any dependencies here if needed
+    description="AWS RDS utility functions for database and table creation through panda dataframes",
+    author="Alex Winters",
+    author_email="awin117@gmail.com",
+    url="https://github.com/ajwinters/rdshelp",  # Optional, if you have a repo
+)
