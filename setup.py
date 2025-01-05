c81b0f93d55139b0aea8480eed01e62e76e21638
Alex Winters <awin117@gmail.com>
2025-01-05 18:28:31 -0500

    update to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rdshelp",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(),
     py_modules=["rdshelp"],  # Name of your Python file
     install_requires=["psycopg2","psycopg2-binary","numpy","pandas"],  # Add any dependencies here if needed
